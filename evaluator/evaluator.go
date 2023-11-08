ca35a285005f6619c7b55ccd36da5687b8791963
myTed <tribune11@nate.com>
2023-11-09 02:11:54 +0900

    boolean evaluator 추가

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,16 +5,31 @@ import (
 	"monkey/object"
 )
 
+var (
+	TRUE = &object.Boolean{Value:true}
+	FALSE = &object.Boolean{Value:false}
+	NULL = &object.NULL{}
+)
+
 func Eval(node ast.Node) object.Object{
 	switch node := node.(type) {
 	case *ast.Program:
 		return loopEval(node.Statements)
 	case *ast.ExpressionStatement:
-		return Eval(node.Expression) 
+		return Eval(node.Expression)
+	case *ast.BooleanExpression:
+		return nativeBooleanObject(node.Value) 
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
 	return nil
+}
+
+func nativeBooleanObject(input bool) *object.Boolean{
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
 
 func loopEval(stmts []ast.Statement) object.Object{
